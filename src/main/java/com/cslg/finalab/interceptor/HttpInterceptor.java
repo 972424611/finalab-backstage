98ea8e95e6b9d6d9aebf5adf0b9ab564078ce444
twilight <972424611@qq.com>
2018-09-04 11:18:21 +0800

    Modify Access-Control-Allow-Origin

--- src/main/java/com/cslg/finalab/interceptor/HttpInterceptor.java
+++ src/main/java/com/cslg/finalab/interceptor/HttpInterceptor.java
@@ -6,14 +6,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @Component
 public class HttpInterceptor implements HandlerInterceptor {
 
+    private static final String[] ALLOW_DOMAIN = {"http://www.finalab.cn", "http://finalab.cn"};
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 允许跨域
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        String originHeader = request.getHeader("Origin");
+        if(Arrays.asList(ALLOW_DOMAIN).contains(originHeader)) {
+            // 允许跨域
+            response.setHeader("Access-Control-Allow-Origin", originHeader);
+        }
+
         // 允许自定义请求头token(允许head跨域)
         //response.setHeader("Access-Control-Allow-Headers", "token, Accept, Origin, X-Requested-With, Content-Type, Last-Modified");
         return true;
